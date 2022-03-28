771075a56c801e6e5a4ab51cd52014b39ad644dd
zerolfx <zerolfx0@gmail.com>
2022-03-27 20:14:35 -0400

    add: login within Emacs

--- copilot.el
+++ copilot.el
@@ -8,6 +8,9 @@
    (or load-file-name
        (buffer-file-name))))
 
+(defconst copilot--request-timeout 5
+  "Timeout for blocking requests to Copilot, in seconds.")
+
 
 (defvar copilot--process nil
   "Copilot agent process")
@@ -71,6 +74,13 @@
           copilot--callbacks)
     (copilot--send-request request)))
 
+(defun copilot--agent-http-request (url params callback)
+  (copilot--agent-request "httpRequest"
+                          (append (list :url url
+                                        :timeout 30000)
+                                  params)
+                          (lambda (result)
+                            (->> result (alist-get 'body) json-read-from-string (cons (cons 'status (alist-get 'status result))) (funcall callback)))))
 
 (defun copilot--process-filter (process output)
   "Process filter for Copilot agent. Only care about responses with id."
@@ -87,12 +97,135 @@
                      json-read-from-string))
            (result (->> body
                         (alist-get 'result)))
+           (err (->> body (alist-get 'error)))
            (id (alist-get 'id body)))
       (setq copilot--output-buffer nil)
       (when (equal id copilot--request-id)
-        (copilot--provide-candidates result (alist-get id copilot--callbacks))
+        (funcall (alist-get id copilot--callbacks) (cons (cons 'error err) result))
         (assq-delete-all id copilot--callbacks)))))
 
+;;
+;; login
+;;
+
+(defconst copilot--client-id "Iv1.b507a08c87ecfe98"
+  "Copilot client id, copied from copilot.vim")
+
+(defconst copilot--config-root
+  (let ((root (concat (or (getenv "XDG_CONFIG_HOME")
+                          (if (eq system-type 'windows-nt)
+                              (expand-file-name "~/Appdata/Local")
+                            (expand-file-name "~/.config")))
+                      "/github-copilot")))
+    (make-directory root t)
+    root)
+  "Copilot config root")
+
+(defconst copilot--config-hosts
+  (concat copilot--config-root "/hosts.json"))
+
+(defun copilot--blocking (f &rest args)
+  (let ((result nil)
+        (count 0))
+    (apply f (append args (list (lambda (r)
+                                  (setq result r)))))
+    (while (and (null result) (< (* 0.1 count) copilot--request-timeout))
+      (sleep-for 0.1)
+      (cl-incf count))
+    result))
+
+(defun copilot-login ()
+  (interactive)
+  (copilot--agent-http-request "https://github.com/login/device/code"
+                               `(:method "POST"
+                                         :headers (:Accept "application/json")
+                                         :json (:client_id ,copilot--client-id :scope "user:read"))
+                               #'copilot--login-callback))
+
+(defun copilot--login-callback (result)
+  (let* ((device-code (alist-get 'device_code result))
+         (verification-uri (alist-get 'verification_uri result))
+         (user-code (alist-get 'user_code result))
+         (interval (alist-get 'interval result)))
+    (when (read-from-minibuffer (format "First copy your one-time code: %s. Press ENTER to continue." user-code))
+      (if (display-graphic-p)
+          (progn
+            (read-from-minibuffer "Press ENTER to open GitHub in your browser")
+            (browse-url verification-uri)
+            (read-from-minibuffer "Press ENTER if you finish authorizing."))
+        (read-from-minibuffer "Please open %s in your browser. Press ENTER if you finish authorizing." verification-uri))
+      (message "Verifying...")
+      (copilot--login-verify device-code))))
+
+
+(defun copilot--login-verify (device-code)
+  (copilot--agent-http-request (format "https://github.com/login/oauth/access_token?grant_type=urn:ietf:params:oauth:grant-type:device_code&device_code=%s&client_id=%s"
+                                       device-code copilot--client-id)
+                               '(:method "GET"
+                                         :headers (:Accept "application/json"))
+                               #'copilot--login-verify-callback))
+
+(defun copilot--login-verify-callback (result)
+  (let ((access-token (alist-get 'access_token result)))
+    (if (not access-token)
+        (message "Login failed.")
+      (let* ((copilot-access-result (copilot--blocking #'copilot--agent-http-request
+                                                       "https://api.github.com/copilot_internal/token"
+                                                       `(:method "GET"
+                                                                 :headers (:Authorization ,(concat "Bearer " access-token)))))
+            (status (alist-get 'status copilot-access-result)))
+        (if (equal status 403)
+            (message "You don't have access to GitHub Copilot. Join the waitlist by visiting https://copilot.github.com")
+          (message "Login success!")
+          (copilot--oauth-save (copilot--oauth-user access-token) access-token))))))
+
+
+(defun copilot--oauth-user (access-token)
+  (let ((result (copilot--blocking #'copilot--agent-http-request
+                                   "https://api.github.com/user"
+                                   `(:method "GET"
+                                             :headers (:Authorization ,(concat "Bearer " access-token))))))
+    (if (not (equal (alist-get 'status result) 200))
+        (message "Failed to get user info.")
+      (alist-get 'login result))))
+
+
+(defun copilot--oauth-save (user access-token)
+  (with-temp-file copilot--config-hosts
+    (insert (json-encode `(:github.com (:user ,user :oauth_token ,access-token))))))
+
+;;
+;; diagnose
+;;
+
+(defun copilot--diagnose-network ()
+  (let ((result (copilot--blocking #'copilot--agent-http-request
+                                   "https://copilot-proxy.githubusercontent.com/_ping"
+                                   '(:method "GET"))))
+    (cond ((not result) "Server connectivity error")
+          ((equal (alist-get 'status result) 466) "Server error")
+          (t "OK"))))
+
+(defun copilot--diagnose-access ()
+  (let* ((result (copilot--blocking #'copilot--agent-request
+                                    "getCompletions"
+                                    '(:doc (:source "" :path "" :relativePath "" :languageId "" :position (:line 0 :character 0)))))
+         (err (alist-get 'error result)))
+    (if err (format "error: %S" err) "OK")))
+
+(defun copilot-diagnose ()
+  "Diagnose copilot."
+  (interactive)
+  (if (not copilot--enabled)
+      (message "Copilot enabled: No"))
+    (message "Copilot enabled: Yes, Network: %s, Access: %s"
+            (copilot--diagnose-network)
+            (copilot--diagnose-access)))
+
+
+;;
+;; Auto completion
+;;
 
 (defconst copilot--language-id
   '((emacs-lisp-mode . "lisp")))
@@ -112,12 +245,6 @@
   (copilot--agent-request "getCompletions"
                           (list :doc (copilot--generate-doc))
                           callback))
-
-
-(defun copilot--provide-candidates (result callback)
-  (let ((completions (alist-get 'completions result)))
-    ;; (message "%S" completions)
-    (funcall callback (if (seq-empty-p completions) nil (seq-elt completions 0)))))
 
 
 (defface copilot-overlay-face
@@ -174,14 +301,16 @@
   (copilot-clear-overlay)
   (when (buffer-file-name)
     (copilot--get-candidates
-     (lambda (completion)
-       (when completion
-         (let* ((text (alist-get 'text completion))
-                (range (alist-get 'range completion))
-                (start (alist-get 'start range))
-                (start-line (alist-get 'line start))
-                (start-char (alist-get 'character start)))
-           (copilot-display-overlay-completion text start-line start-char)))))))
+     (lambda (result)
+       (let* ((completions (alist-get 'completions result))
+              (completion (if (seq-empty-p completions) nil (seq-elt completions 0))))
+        (when completion
+          (let* ((text (alist-get 'text completion))
+                 (range (alist-get 'range completion))
+                 (start (alist-get 'start range))
+                 (start-line (alist-get 'line start))
+                 (start-char (alist-get 'character start)))
+            (copilot-display-overlay-completion text start-line start-char))))))))
 
 
 (defconst copilot--hooks
@@ -196,6 +325,7 @@
 (defun copilot-enable ()
   (interactive)
   (unless copilot--enabled
+    (copilot--start-process)
     (dolist (hook copilot--hooks)
       (add-hook (car hook) (cadr hook)))
     (setq copilot--enabled t)))

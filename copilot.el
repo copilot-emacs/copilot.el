d47c7c603c1dd24d28a33a933df93764f3d458d5
Alexander Arvidsson <alexander@arvidson.nu>
2023-12-29 19:15:52 +0100

    fix: Bump required jsonrpc to 1.0.23

--- copilot.el
+++ copilot.el
@@ -1,6 +1,6 @@
 ;;; copilot.el --- An unofficial Copilot plugin for Emacs  -*- lexical-binding:t -*-
 
-;; Package-Requires: ((emacs "27.2") (s "1.12.0") (dash "2.19.1") (editorconfig "0.8.2") (jsonrpc "1.0.22"))
+;; Package-Requires: ((emacs "27.2") (s "1.12.0") (dash "2.19.1") (editorconfig "0.8.2") (jsonrpc "1.0.23"))
 
 ;;; Code:
 (require 'cl-lib)

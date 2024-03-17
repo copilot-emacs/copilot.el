ee4eca6d545f506eb5d01393c0fca0e6b30d7cce
JenChieh <jcs090218@gmail.com>
2024-03-16 19:19:36 -0700

    Add headers

--- copilot.el
+++ copilot.el
@@ -1,8 +1,35 @@
-;;; copilot.el --- An unofficial Copilot plugin for Emacs  -*- lexical-binding:t -*-
+;;; copilot.el --- An unofficial Copilot plugin for Emacs  -*- lexical-binding: t; -*-
 
+;; Copyright (C) 2022-2024  copilot-emacs maintainers
+
+;; Author: zerol <z@zerol.me>
+;; Maintainer: Emil van der Westhuizen
+;;             Shen, Jen-Chieh <jcs090218@gmail.com>
+;;             Rakotomandimby Mihamina <mihamina.rakotomandimby@rktmb.org>
+;; URL: https://github.com/copilot-emacs/copilot.el
 ;; Package-Requires: ((emacs "27.2") (s "1.12.0") (dash "2.19.1") (editorconfig "0.8.2") (jsonrpc "1.0.14") (f "0.20.0"))
 ;; Version: 0.0.1
-;; URL: https://github.com/copilot-emacs/copilot.el
+;; Keywords: convenience copilot
+
+;; The MIT License (MIT)
+
+;; Permission is hereby granted, free of charge, to any person obtaining a copy
+;; of this software and associated documentation files (the "Software"), to deal
+;; in the Software without restriction, including without limitation the rights
+;; to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+;; copies of the Software, and to permit persons to whom the Software is
+;; furnished to do so, subject to the following conditions:
+
+;; The above copyright notice and this permission notice shall be included in all
+;; copies or substantial portions of the Software.
+
+;; THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+;; IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+;; FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+;; AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+;; LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+;; OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+;; SOFTWARE.
 
 ;;; Commentary:
 
